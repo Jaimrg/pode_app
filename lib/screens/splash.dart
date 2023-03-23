d4370ec48f65d176402aef7b19b154275ead057b
Jaime RG <75501289+Jaimrg@users.noreply.github.com>
2023-03-23 22:36:51 +0200

    splash screen done

---
+++ lib/screens/splash.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(50, 140, 115, 1),
+      body: Center(
+        child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'images/logoPode.png',
+                      height: 270,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Expanded(child: Padding(padding: EdgeInsets.only(top: 20,right: 50,left: 50),
+                    child: RichText(
+                      textAlign: TextAlign.center,
+                      text: const TextSpan(
+                        text:
+                            'Being a Person With a disability does not mean  being less qualified\nYou ',
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal, fontSize: 20,color: Colors.white,fontFamily: 'Montserrat'),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: 'Can!',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20.0,color: Colors.white, fontFamily: 'Montserrat')),
+                          
+                        ],
+                      ),
+                    ),)),                  
+                  ],
+                )
+              ],
+            )),
+      ),
+    );
+  }
+}
